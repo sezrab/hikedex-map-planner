61656e6349e2581a88fe42f70b764e82f1c10891
Sam Berry <samuel.ezra.berry@gmail.com>
2025-06-25 17:09:44 +0100

    fix: update Navbar component to include logo and improve layout structure

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,21 @@
-import { Anchor, Button, Group, Title } from '@mantine/core';
+import { Anchor, Button, Group, Title, Image } from '@mantine/core';
 
 export default function Navbar({ page }: { page?: string }) {
     return (
-        <Group mb='lg' align="center" style={{ flexWrap: 'wrap' }}>
-            <Anchor
-                href="/"
-                style={{ textDecoration: 'none', flex: 1 }}>
-                <Title order={3} c="indigo.9" display={{ base: 'none', xs: 'block' }}>
-                    hikedex
-                </Title>
+        <Group mb='lg' align="center" style={{ flexWrap: 'wrap' }} >
+            <Anchor href="/" display={{ base: 'none', xs: 'block' }} style={{ textDecoration: 'none', flex: 1, flexDirection: 'row' }}>
+                <Group gap='md' align="center" style={{ textDecoration: 'none', color: 'inherit', alignItems: 'center' }}>
+                    <img
+                        src="/logo.svg"
+                        alt="Hikedex Logo"
+                        height={40}
+                        width={40}
+                        style={{ marginTop: 5 }}
+                    />
+                    <Title order={3} c="indigo.8">
+                        hikedex
+                    </Title>
+                </Group>
             </Anchor>
             <Button component="a" href="/map" variant="subtle" color="indigo" radius="sm">
                 map
@@ -25,6 +32,6 @@ export default function Navbar({ page }: { page?: string }) {
             >
                 sunrise radar
             </Button>
-        </Group>
+        </Group >
     );
 }
